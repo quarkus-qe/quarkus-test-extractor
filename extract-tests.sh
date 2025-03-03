03602612217fdfafe50b729af2280e6eca5b253a
Michal Vavřík <mvavrik@redhat.com>
2025-03-03 23:44:51 +0100

    Implement opening of PRs for extracted tests

--- extract-tests.sh
+++ extract-tests.sh
@@ -1,46 +1,55 @@
+#!/bin/bash
+
 QUARKUS_URL='git@github.com:quarkusio/quarkus.git'
-QUARKUS_BRANCH='main'
+QUARKUS_GIT_CHECKOUT='main'
 QUARKUS_SOURCE_DIR=$PWD
 VERBOSE=false
 TARGET_DIR='/tmp/extracted-tests'
-EXTRACTED_TESTS_PROJECT_URL='git@github.com:quarkus-qe/quarkus-extracted-tests.git'
+EXTRACTED_TESTS_PROJECT='quarkus-qe/quarkus-extracted-tests'
 PUSH_EXTRACTED_TESTS=false
 WORKING_DIR='/tmp/test-extractor'
 SUPER_VERBOSE=false
+SKIP_QUARKUS_BUILD=false
 
-while getopts l:b:d:vt:u:n:pxw: opt
+while getopts l:b:d:vt:u:n:pxw:s opt
 do
     case "${opt}" in
         l) QUARKUS_URL=${OPTARG};;
-        b) QUARKUS_BRANCH=${OPTARG};;
+        b) QUARKUS_GIT_CHECKOUT=${OPTARG};;
         d) QUARKUS_SOURCE_DIR=${OPTARG};;
         v) VERBOSE=true;;
         t) TARGET_DIR=${OPTARG};;
-        u) EXTRACTED_TESTS_PROJECT_URL=${OPTARG};;
+        u) EXTRACTED_TESTS_PROJECT=${OPTARG};;
         n) EXTRACTED_TESTS_PROJECT_BRANCH=${OPTARG};;
         p) PUSH_EXTRACTED_TESTS=true;;
         w) WORKING_DIR=${OPTARG};;
         x) SUPER_VERBOSE=true;;
+        s) SKIP_QUARKUS_BUILD=true;;
     esac
 done
 
 # enforce options defaults
 if [ "${EXTRACTED_TESTS_PROJECT_BRANCH+set}" != set ]; then
-  EXTRACTED_TESTS_PROJECT_BRANCH="${QUARKUS_BRANCH}"
+  EXTRACTED_TESTS_PROJECT_BRANCH="${QUARKUS_GIT_CHECKOUT}"
 fi
 if [ "$SUPER_VERBOSE" = true ]; then
   VERBOSE=true
+fi
+
+# print out commands for better understanding of what is happening
+if [ "$VERBOSE" = true ]; then
+  set -x
 fi
 
 # inform about script configuration
 if [ "$VERBOSE" = true ]; then
     echo 'Running in a verbose mode with following configuration:'
     echo '- Quarkus project URL:' $QUARKUS_URL
-    echo '- Quarkus project branch:' $QUARKUS_BRANCH
+    echo '- Quarkus project branch or tag:' $QUARKUS_GIT_CHECKOUT
     echo '- Quarkus project source directory:' $QUARKUS_SOURCE_DIR
     echo '- Test extraction result will be placed in' $TARGET_DIR
     if [ "$PUSH_EXTRACTED_TESTS" = true ] ; then
-      echo '- Extracted tests will be pushed to the project with URL' $EXTRACTED_TESTS_PROJECT_URL 'and branch' $EXTRACTED_TESTS_PROJECT_BRANCH
+      echo '- Extracted tests will be pushed to the project' $EXTRACTED_TESTS_PROJECT 'and branch' $EXTRACTED_TESTS_PROJECT_BRANCH
     fi
 fi
 
@@ -51,7 +60,7 @@ exitCode=$?
 if [ $exitCode -ne 0 ]; then
     mkdir -p $WORKING_DIR
     cd $WORKING_DIR
-    git clone git@github.com:quarkus-qe/quarkus-test-extractor.git
+    git clone git@github.com:quarkus-qe/quarkus-test-extractor.git --depth=1
     cd quarkus-test-extractor
     mvn clean install -DskipTests -DskipITs
     cd $PREVIOUS_DIR
@@ -65,14 +74,27 @@ cd $QUARKUS_SOURCE_DIR
 
 # if Quarkus source is not available, get it
 if ! test -f './pom.xml'; then
-  git clone $QUARKUS_URL -b $QUARKUS_BRANCH
+  git clone $QUARKUS_URL --depth=1
   cd quarkus
-elif [ "$VERBOSE" = true ]; then
-  echo 'Detected POM file, assuming Quarkus project already exists'
+  git fetch --tags
+  git checkout -B $QUARKUS_GIT_CHECKOUT $QUARKUS_GIT_CHECKOUT
+else
+  if [ $QUARKUS_GIT_CHECKOUT != 'main' ]; then
+    QUARKUS_GIT_CHECKOUT="$(git rev-parse --abbrev-ref HEAD)"
+  else
+    git fetch --tags
+    git checkout -B $QUARKUS_GIT_CHECKOUT $QUARKUS_GIT_CHECKOUT
+  fi
+  if [ "$VERBOSE" = true ]; then
+    echo 'Detected POM file and git tag or branch' $QUARKUS_GIT_CHECKOUT ', assuming Quarkus project already exists'
+  fi
 fi
 
+# save git HEAD
+QUARKUS_GIT_HEAD=$(git rev-parse --short HEAD)
+
 # recreate directory with extraction results
-rm $TARGET_DIR -r || true > /dev/null
+rm $TARGET_DIR -r -f || true > /dev/null
 mkdir -p $TARGET_DIR
 
 # use auxiliary file to filter how much information is logged
@@ -87,12 +109,38 @@ if [ "$SUPER_VERBOSE" = true ]; then
   ADDITIONAL_ARGS='-X'
 fi
 
+# if we also want to push the extracted tests, we need to prepare git project
+if [ "$PUSH_EXTRACTED_TESTS" = true ]; then
+  echo 'Preparing' $EXTRACTED_TESTS_PROJECT 'project'
+  PREVIOUS_DIR=$PWD
+  cd $TARGET_DIR/..
+  # get target dir name (remainder after the last path separator)
+  TARGET_DIR_NAME=${TARGET_DIR##*/ }
+  gh repo clone $EXTRACTED_TESTS_PROJECT $TARGET_DIR_NAME -- --depth=1
+  cd $TARGET_DIR_NAME
+  git checkout -B $EXTRACTED_TESTS_PROJECT_BRANCH
+  if [ -n "$(ls -A . 2>/dev/null)" ]
+  then
+    # remove all files as we will provide a new version (we will extract new tests)
+    git rm -r *
+  fi
+  cd $PREVIOUS_DIR
+fi
+
+# build Quarkus because some artifacts are not available in Maven central
+if [ "$SKIP_QUARKUS_BUILD" = false ]; then
+  echo 'Building Quarkus'
+  MAVEN_OPTS="-Xmx4g" ./mvnw -Dquickly
+else
+  echo 'Skipping Quarkus build'
+fi
+
 # collect metadata about Quarkus BOM
 echo 'Collecting metadata about Quarkus BOM'
 mvn -f bom/application/ io.quarkus.qe:quarkus-test-extractor:1.0-SNAPSHOT:parse-quarkus-bom -Dwrite-to=$TARGET_DIR $ADDITIONAL_ARGS >> test-extraction-log
 
 # extract tests
-echo 'Extracting tests, you will be informed about the extraction result'
+echo 'Extracting tests from Quarkus ' $QUARKUS_GIT_HEAD ', you will be informed about the extraction result'
 mvn io.quarkus.qe:quarkus-test-extractor:1.0-SNAPSHOT:extract-tests -Dwrite-to=$TARGET_DIR $ADDITIONAL_ARGS >> test-extraction-log
 
 # detect whether the extraction succeeded
@@ -112,21 +160,28 @@ fi
 cd $TARGET_DIR
 
 # inform about extraction details
-cat extraction-summary
+if [ "$VERBOSE" = true ]; then
+  cat extraction-summary
+fi
 
-# delete auxiliary files and directories
-rm -r extraction-summary quarkus-bom-managed-deps quarkus-build-parent-context quarkus-parent-pom-context partial-extraction-summaries
+# delete auxiliary files and directories, but keep 'extraction-summary' as it might be useful
+rm -r -f quarkus-bom-managed-deps quarkus-build-parent-context quarkus-parent-pom-context partial-extraction-summaries
 
 # push extracted tests to dedicated GitHub project
 if [ "$PUSH_EXTRACTED_TESTS" = true ]; then
-  echo 'Pushing extracted tests to' $EXTRACTED_TESTS_PROJECT_URL 'branch' $EXTRACTED_TESTS_PROJECT_BRANCH
-  # FIXME: before extraction begins, we need to check out the project, the branch and delete content
-  #   now we need to just add all things to git and open PR
+  git add *
+  GIT_COMMIT_OUTPUT=$(git commit -am "Add tests extracted from $QUARKUS_GIT_HEAD")
+  if [[ $GIT_COMMIT_OUTPUT == *"nothing to commit"* ]]; then
+    echo 'Nothing to push as there were no relevant changes'
+    exit 0
+  fi
+  if [ "$VERBOSE" = true ]; then
+    git status
+  fi
+  git checkout -b "temporary-branch/pr/$QUARKUS_GIT_CHECKOUT"
+  git push origin "temporary-branch/pr/$QUARKUS_GIT_CHECKOUT"
+  echo 'Opening PR in project' $EXTRACTED_TESTS_PROJECT
+  PR_MESSAGE="Adding tests extracted from $QUARKUS_URL $QUARKUS_GIT_CHECKOUT with HEAD on $QUARKUS_GIT_HEAD"
+  PR_TITLE="Add new tests extracted from $QUARKUS_GIT_CHECKOUT tag"
+  gh pr create -a '@me' -r 'QuarkusQE' -b "$PR_MESSAGE" -t "$PR_TITLE" -B "$EXTRACTED_TESTS_PROJECT_BRANCH" -R "$EXTRACTED_TESTS_PROJECT"
 fi
-
-# FIXME: store logs as job artifacts
-# FIXME: store extraction summary as artifact and print it out after execution
-# FIXME: do something with extracted tests, e.g. push them to configured repo (and allow to configure repo)
-# FIXME: probably open PR and auto-merge it when CI is green, if CI is red, send an e-mail
-# FIXME: ideally this script would be triggered by a release or manually
-# FIXME: document how this script is triggered
