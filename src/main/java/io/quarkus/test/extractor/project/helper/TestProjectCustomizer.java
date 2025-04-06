0cbc494454762e81435c3b56243bece1ad0a2470
Michal Vavřík <mvavrik@redhat.com>
2025-04-06 12:36:32 +0200

    Fix RemovedResourceFastJarTest in packaging IT module

--- src/main/java/io/quarkus/test/extractor/project/helper/TestProjectCustomizer.java
+++ src/main/java/io/quarkus/test/extractor/project/helper/TestProjectCustomizer.java
@@ -1,19 +1,20 @@
 package io.quarkus.test.extractor.project.helper;
 
 import io.quarkus.test.extractor.project.builder.Project;
-import io.quarkus.test.extractor.project.utils.PluginUtils;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
-import org.apache.maven.model.Plugin;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 import static io.quarkus.test.extractor.project.utils.PluginUtils.getTargetProjectDirPath;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 public abstract class TestProjectCustomizer {
 
@@ -23,7 +24,44 @@ public abstract class TestProjectCustomizer {
         CUSTOMIZERS = Map.of("quarkus-websockets-next-deployment", createWebSocketsNextKotlinCustomizer(),
                 "quarkus-integration-test-devtools", createDevToolsItModuleCustomizer(),
                 "quarkus-integration-test-rest-client-reactive-kotlin-serialization-with-validator",
-                createKotlinSerializationWithValidatorCustomizer());
+                createKotlinSerializationWithValidatorCustomizer(),
+                "quarkus-integration-test-packaging", createPackagingItModuleCustomizer());
+    }
+
+    private TestProjectCustomizer() {
+
+    }
+
+    protected abstract void customize(Project project, Model model);
+
+    public static void customizeIfNecessary(Project project, Model model) {
+        final TestProjectCustomizer testProjectCustomizer = CUSTOMIZERS.get(project.artifactId());
+        if (testProjectCustomizer != null) {
+            testProjectCustomizer.customize(project, model);
+        }
+    }
+
+    private static TestProjectCustomizer createPackagingItModuleCustomizer() {
+        return new TestProjectCustomizer() {
+            @Override
+            protected void customize(Project project, Model model) {
+                // we had to change test parent group id from 'io.quarkus' to 'io.quarkus.qe.tests',
+                // mainly due to extensions modules, as if we want to test for example Quarkus Vert.x HTTP extension
+                // tests, then we need 'io.quarkus:quarkus-vertx-http-deployment' inside extension modules because
+                // some tests customize build steps etc., but tests for that extensions are also placed in the module
+                // with the same artifact, so if we extracted tests into a Maven module with the same GAV
+                // we would be testing our local sources and not RHBQ bits; on the other hand, we cannot just change
+                // artifact id of a module where we place extracted tests because many tests has hardcoded expectations
+                // related to the artifact id with which we execute these tests (e.g. Flyway migration file name)
+                // so instead we have just a different group id for our extracted test modules, which is why we need to
+                // change the 'quarkus.class-loading.removed-resources'."io.quarkus\:quarkus-integration-test-shared-library"
+                // configuration property value to the one with the 'io.quarkus.qe.tests' group id instead
+                var fileChanger = new FileChanger(project, "src/test/java/io/quarkus/removedclasses/AbstractRemovedResourceTest.java");
+                fileChanger.changeContent(testContent -> testContent.replace(
+                        "io.quarkus\\\\:quarkus-integration-test-shared-library",
+                        "io.quarkus.qe.tests\\\\:quarkus-integration-test-shared-library"));
+            }
+        };
     }
 
     private static TestProjectCustomizer createKotlinSerializationWithValidatorCustomizer() {
@@ -36,19 +74,8 @@ public abstract class TestProjectCustomizer {
                 // I suspect there is some library missing or plugin missing or wrong path to classes,
                 // but I failed to figure which one
                 // TODO: if we start supporting Kotlin, this needs to be debugged
-                var basicTestPath = getTargetProjectDirPath(project).resolve("src/test/kotlin/io/quarkus/it/rest/client/BasicTest.kt");
-                if (Files.exists(basicTestPath)) {
-                    try {
-                        String testContent = Files.readString(basicTestPath);
-                        testContent =  testContent.replace("validate.id", "validate.arg0");
-                        Files.writeString(basicTestPath, testContent);
-                    } catch (IOException e) {
-                        throw new RuntimeException("Failed to adjust file " + basicTestPath, e);
-                    }
-                } else {
-                    // this is not super important, but we should at least warn
-                    System.err.println("Failed to find file " + basicTestPath + " which means implementation has changed");
-                }
+                var fileChanger = new FileChanger(project, "src/test/kotlin/io/quarkus/it/rest/client/BasicTest.kt");
+                fileChanger.changeContent(testContent -> testContent.replace("validate.id", "validate.arg0"));
             }
         };
     }
@@ -79,30 +106,30 @@ public abstract class TestProjectCustomizer {
             @Override
             protected void customize(Project project, Model model) {
                 // required fix for io.quarkus.devtools.commands.CreateProjectPlatformMetadataTest
-                String platformMetadataJson = getTargetProjectDirPath(project)
-                        .resolve("src/test/resources/platform-metadata.json")
-                        .toAbsolutePath()
-                        .toString();
-                // otherwise this is resolved to 'io.quarkus.qe.tests' instead of 'io.quarkus'
-                // but we cannot afford to use 'io.quarkus' for our module group ids
-                String fileContent = FileSystemStorage.loadFileContent(platformMetadataJson)
+                var fileChanger = new FileChanger(project, "src/test/resources/platform-metadata.json");
+                fileChanger.changeContent(fileContent -> fileContent
                         .replaceAll(Pattern.quote("{project.version}"), "{quarkus.platform.version}")
-                        .replaceAll(Pattern.quote("{project.groupId}"), "{quarkus.platform.group-id}");
-                FileSystemStorage.replaceFileContent(platformMetadataJson, fileContent);
+                        .replaceAll(Pattern.quote("{project.groupId}"), "{quarkus.platform.group-id}"));
             }
         };
     }
 
-    private TestProjectCustomizer() {
+    private record FileChanger(Project project, String filePath) {
 
-    }
-
-    protected abstract void customize(Project project, Model model);
-
-    public static void customizeIfNecessary(Project project, Model model) {
-        final TestProjectCustomizer testProjectCustomizer = CUSTOMIZERS.get(project.artifactId());
-        if (testProjectCustomizer != null) {
-            testProjectCustomizer.customize(project, model);
+        private void changeContent(Function<String, String> replacement) {
+            // this is the right way to get path for ITs, I didn't try it for extension modules
+            var absolutePath = getTargetProjectDirPath(project).resolve(filePath);
+            if (Files.exists(absolutePath)) {
+                try {
+                    String testContent = Files.readString(absolutePath);
+                    Files.writeString(absolutePath, replacement.apply(testContent), TRUNCATE_EXISTING);
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to adjust file " + absolutePath, e);
+                }
+            } else {
+                // this is not super important, but we should at least warn
+                System.err.println("Failed to find file " + absolutePath + " which means implementation has changed");
+            }
         }
     }
 }
